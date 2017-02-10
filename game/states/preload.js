95c7e684e659091e26dcabe18aba2697cb10f629
thomasvandewaeter <thomasvandewaeter@hotmail.com>
2017-02-10 23:25:17 +0100

    Add files via upload

--- game/states/preload.js
+++ game/states/preload.js
@@ -15,7 +15,7 @@ Preload.prototype = {
     this.load.image('background', 'assets/background_blue.png');
     this.load.image('ground', 'assets/ground.png');
     this.load.image('title', 'assets/title.png');
-    this.load.spritesheet('bird', 'assets/bird.png', 34,24,3);
+    this.load.spritesheet('bird', 'assets/bird_new.png', 34,24,3);
     this.load.spritesheet('pipe', 'assets/pipes.png', 54,320,2);
     this.load.spritesheet('ground_pipe', 'assets/ground_pipe.png', 12,320,2);
     this.load.image('startButton', 'assets/start-button.png');
